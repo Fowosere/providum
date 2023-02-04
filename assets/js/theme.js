e2e9c89e83f58c8e7199487f6d48b650f7808433
Fowosere <you@example.com>
2023-02-04 05:03:39 +0100

    added icons for fav-icon

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -857,6 +857,26 @@ var zanimationInit = function zanimationInit() {
 /* -------------------------------------------------------------------------- */
 
 
+
+
+(function ($) {
+
+  "use strict";
+
+  $('nav .dropdown').hover(function () {
+    var $this = $(this);
+    $this.addClass('show');
+    $this.find('> a').attr('aria-expanded', true);
+    $this.find('.dropdown-menu').addClass('show');
+  }, function () {
+    var $this = $(this);
+    $this.removeClass('show');
+    $this.find('> a').attr('aria-expanded', false);
+    $this.find('.dropdown-menu').removeClass('show');
+  });
+
+})(jQuery);
+
 docReady(navbarInit);
 docReady(detectorInit);
 docReady(zanimationInit);
