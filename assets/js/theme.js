2a08ca2b622925522e77e49081063395272cc38c
Fowosere <you@example.com>
2023-02-04 08:59:26 +0100

    adding submenu to the menu

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -877,6 +877,27 @@ var zanimationInit = function zanimationInit() {
 
 })(jQuery);
 
+
+(function ($) {
+
+  "use strict";
+
+  $('nav .dropdown').hover(function () {
+    var $this = $(this);
+    $this.addClass('show');
+    $this.find('> a').attr('aria-expanded', true);
+    $this.find('.dropdown-menu').addClass('show');
+  }, function () {
+    var $this = $(this);
+    $this.removeClass('show');
+    $this.find('> a').attr('aria-expanded', false);
+    $this.find('.dropdown-menu').removeClass('show');
+  });
+
+})(jQuery);
+
+
+
 docReady(navbarInit);
 docReady(detectorInit);
 docReady(zanimationInit);
